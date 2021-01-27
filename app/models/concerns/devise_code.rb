1826be4ecdebb2734e4e33e6777b28c51da4ac66
logictkt <logictkt@gmail.com>
2021-01-27 13:38:21 +0900

    fix

--- app/models/concerns/devise_code.rb
+++ app/models/concerns/devise_code.rb
@@ -4,21 +4,6 @@ module DeviseCode
   included do
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable,
-           :confirmable, :lockable, :timeoutable, :trackable, authentication_keys: [:login]
-
-    attr_writer :login
-
-    def login
-      @login || self.name || self.email
-    end
-  end
-
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:name) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
+           :confirmable, :lockable, :timeoutable, :trackable
   end
 end
