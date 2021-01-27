3a549716dfd59822bcff6d6e31768bc546da4d5d
logictkt <logictkt@gmail.com>
2021-01-27 14:14:56 +0900

    fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-
-// Vue.js フル版（Compiler入り）
-environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
-
-// ここから
 // jQueryとBootstapのJSを使えるように
 const webpack = require('webpack')
 environment.plugins.prepend(
@@ -19,6 +10,5 @@ environment.plugins.prepend(
     Popper: 'popper.js'
   })
 )
-// ここまで
 
 module.exports = environment
